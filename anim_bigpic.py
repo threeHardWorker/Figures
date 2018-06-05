097872e64597af6bac06198372ff2f9940f12695
SeanGo <stoneguo@126.com>
2018-06-05 21:05:56 +0800

    title

--- anim_bigpic.py
+++ anim_bigpic.py
@@ -33,7 +33,7 @@ class SubplotAnimation9:
         self.region = [par.Rect() for _ in range(12)]
         self.fig.tight_layout()
         self.params = params
-        self.fig.canvas.set_window_title(params.inst + ' m12')
+        self.fig.canvas.set_window_title(params.inst + ' m12 - 10s')
 
         self.m12 = m12
         self.dcplp = dcplp
@@ -59,7 +59,7 @@ class SubplotAnimation9:
         self.fig2 = plt.figure(1, figsize=(16, 9))
         self.ax2 = self.fig2.add_subplot(1, 1, 1)
         self.fig2.tight_layout()
-        self.fig2.canvas.set_window_title(params.inst + ' bigpic')
+        self.fig2.canvas.set_window_title(params.inst + ' bigpic - 10s')
         self.region2 = par.Rect()
 
         self.art2 = artist_bigpic.ArtistBigPicture(m12, dcplp, self.ax2, "bigpic", 7)
--- anim_bigpic_ctpmdif.py
+++ anim_bigpic_ctpmdif.py
@@ -29,7 +29,7 @@ class SubplotAnimation9:
         self.region = [par.Rect() for _ in range(12)]
         self.fig.tight_layout()
         self.params = params
-        self.fig.canvas.set_window_title(params.inst + ' m12')
+        self.fig.canvas.set_window_title(params.inst + ' m12 - tick')
 
         self.m12 = m12
         self.dcplp = dcplp
@@ -56,7 +56,7 @@ class SubplotAnimation9:
         self.fig2 = plt.figure(1, figsize=(16, 9))
         self.ax2 = self.fig2.add_subplot(1, 1, 1)
         self.fig2.tight_layout()
-        self.fig2.canvas.set_window_title(params.inst + ' bigpic')
+        self.fig2.canvas.set_window_title(params.inst + ' bigpic - tick')
         self.region2 = par.Rect()
 
         self.art2 = artist_bigpic_ctpmdif.ArtistBigPicture(
