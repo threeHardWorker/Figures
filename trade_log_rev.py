712fa76105ef1a173ff55ef19201c1ba3b886cf4
SeanGo <stoneguo@126.com>
2018-06-25 21:49:49 +0800

    something

--- trade_log_rev.py
+++ trade_log_rev.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import gc, sys
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     cls_rev = []
     rev = []
     r = 0.0
+    str_pair = []
 
     instrument = sys.argv[1].encode('ascii')
     version = sys.argv[3].encode('ascii')
@@ -27,12 +29,29 @@ if __name__ == "__main__":
             s = line.split(',')
             if len(s) == 11:
                 x.append(float(s[3]))
-                r += float(s[7])
-                cls_rev.append(float(s[7]))
+                c_rev = float(s[7])
+                r += c_rev
+                cls_rev.append(c_rev)
                 rev.append(r)
+                str_time = \
+                    datetime.fromtimestamp(float(s[10])).strftime("%y-%m-%d %H:%M:%S")
+                pair = '[\"%s\",%.02f]' % (str_time, r)
+                str_pair.append(pair)
 
     plt.plot(x, rev)
     plt.show()
 
     plt.plot(x, cls_rev)
     plt.show()
+
+    datapath = 'data.echats-' + instrument
+    with open(datapath, 'w') as fp:
+        fp.write('[')
+        fp.write(str_pair[0])
+        size = len(str_pair)
+        i = 1
+        while i < size:
+            fp.write(',')
+            fp.write(str_pair[i])
+            i += 1
+        fp.write(']')
